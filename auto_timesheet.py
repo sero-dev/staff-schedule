391bfbe887a31e793eb34981f975f2c6a191d5ab
Sean Rodriguez <sean.rodriguez@outlook.com>
2017-07-25 20:39:58 -0400

    Changes auto_timesheet to fit into Google's Event JSON file

--- auto_timesheet.py
+++ auto_timesheet.py
@@ -4,31 +4,55 @@ import os
 path = os.getcwd();
 
 def getStart(period):
-    if(int(period) < 12):
-        return(' AM')
+    hour = int(period.rsplit(':', 1)[0])
+
+    if(hour > 7 and hour < 13):
+        return str(hour) + ':' + period.rsplit(':', 1)[1] + ':00'
     else:
-        return(' PM')
+        return str(hour + 12) + ':' + period.rsplit(':', 1)[1] + ':00'
 
 def getEnd(period):
-    if(int(period) > 9 and int(period) < 12):
-        return (' AM')
+    hour = int(period.rsplit(':', 1)[0])
+
+    if(hour > 9 and hour < 13):
+        return str(hour) + ':' + period.rsplit(':', 1)[1] + ':00'
     else:
-        return (' PM')
+        return str(hour + 12) + ':' + period.rsplit(':', 1)[1] + ':00'
 
-def convertTime(week, from):
-      if(not(week[from].value == None or week[from].value == "OFF")):
-     = (week[from].value.rsplit('-', 1)[0]
-          + getPeriod1(week[from].value.rsplit('-', 1)[0].rsplit(':', 1)[0]))
-     = (week[from].value.rsplit('-', 1)[1]                            # Change 'to' in a way that moves over two columns
-          + getPeriod2(week[from].value.rsplit('-', 1)[1].rsplit(':', 1)[0]))
+def convertTime(week, day):
+      if not (week[day].value == None or week[day].value == "OFF"):
+          start = getStart(week[day].value.rsplit('-', 1)[0])
+          end = getEnd(week[day].value.rsplit('-', 1)[1])
 
-w1 = openpyxl.load_workbook('week.xlsx')
-w1 = w1.get_sheet_by_name('Sheet1')
+          print(start, '-', end)
 
-convertTime(w1,'C16')
-convertTime(w1,'E16')
-convertTime(w1,'G16')
-convertTime(w1,'I16')
-convertTime(w1,'K16')
-convertTime(w1,'M16')
-convertTime(w1,'O16')
+          eventInfo = {
+            'summary': 'QBPL Cyber Center',
+            'location': 'Queens Library (Central) 89-11 Merrick Blvd, Jamaica, NY 11432',
+            'start': {
+              'dateTime': '2017-07-26T' + start + '-04:00',
+              'timeZone': 'America/New_York',
+            },
+            'end': {
+              'dateTime': '2017-07-26T' + end + '-04:00',
+              'timeZone': 'America/New_York',
+            },
+            'reminders': {
+              'useDefault': False,
+              'overrides': [
+                {'method': 'popup', 'minutes': 60},
+              ],
+            },
+          }
+
+          print(eventInfo)
+
+week = openpyxl.load_workbook('week.xlsx')
+week = week.get_sheet_by_name('Sheet1')
+
+convertTime(week,'E16')
+convertTime(week,'G16')
+convertTime(week,'I16')
+convertTime(week,'K16')
+convertTime(week,'M16')
+convertTime(week,'O16')
